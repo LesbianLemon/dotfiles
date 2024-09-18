1ea6220e70d3b50b9700fa296d908e09a548dc12
Andrej Anžlovar <andrej.anzlovar2@gmail.com>
2024-09-18 18:54:19 +0200

    Updated colorscheme name

--- dot_config/nvim/lua/plugins/colorschemes.lua
+++ dot_config/nvim/lua/plugins/colorschemes.lua
@@ -3,7 +3,7 @@ return {
 		"christianchiarulli/nvcode-color-schemes.vim",
 	},
 	{
-		dir = "~/Programming/snazzy-lemon.vim",
+		dir = "~/Programming/snazzy-lemon.nvim",
 		priority = 1000,
 		config = function()
 			vim.cmd([[colorscheme snazzy-lemon]])
